c5424a5e4792a5b6a43955c7e7ca6d8c75a32dd9
Ilya Moskvin <ilostbe@gmail.com>
2023-09-18 08:21:55 +0300

    1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Открыть магазин', web_app=WebAppInfo(url='https://github.com/champagnedevil/tgstore2')))
+    markup.add(types.KeyboardButton('Открыть магазин', web_app=WebAppInfo(url='https://champagnedevil.github.io/')))
     await message.answer('Добро пожаловать в наш магазин', reply_markup=markup)
 
 executor.start_polling(dp)
